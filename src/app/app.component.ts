1f7a60903b04a7d3309935690bee5afe2db7f2b6
srkreddydendi <srkreddydendi@gmail.com>
2020-03-20 23:39:07 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,6 +19,14 @@ export class AppComponent {
 
       if (k == 22) {
         //  k=0;
+        if(this.name.charAt(i)!= ""){
+          let lastIndex = this.name.lastIndexOf(" ");
+          this.name.substring(0, lastIndex-1)+ "\n" + this.name.substring(lastIndex, this.name.length);
+
+        }else{
+          this.name.concat("\n");
+        }
+        this.finalText = this.name;
         console.log(k);
         console.log(name);
         k = 0;
