c00e411994142ff7e5582df3d0a7273ab942808c
srkreddydendi <srkreddydendi@gmail.com>
2020-03-20 22:34:09 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name : string = 'Angular 6';
-  finalText='';
-  ouput='';
+export class AppComponent {
+  name: string = "Angular 6";
+  finalText = "";
+  ouput = "";
   colLenght = 22;
-  print(){
-let k=0;
-  for(let i=0; i< this.name.length ; i++){
-  //  console.log(this.name);
-    console.log(k);
-   
-    if(k== 22  ){
-  console.log(this.name);
-  k=0;
-    }else {
-      k++;
+  k:number = 0;
+  print() {
+    
+    for (let i = 0; i < this.name.length; i++) {
+      //  console.log(this.name);
+      //  console.log(k);
+
+      if (this.k == 22) {
+        //  k=0;
+        console.log(this.k);
+        console.log(this.name);
+        this.k = 0;
+      } else {
+        this.k=i;
+      }
     }
-  }
-  
   }
 }
