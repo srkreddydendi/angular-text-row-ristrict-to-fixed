2c33c479fa4817f1821d6332745dd50d50323c78
srkreddydendi <srkreddydendi@gmail.com>
2020-03-23 23:03:09 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,31 +7,34 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   name: string = "Angular 6";
-  finalText = "";
+  
   ouput = "";
   colLenght = 22;
   //k:number = 0;
   print() {
     let k=0;
     let line =1;
+   let  finalText = "";
     let localName = this.name;
+    localName = localName.replace(/\s/g, " ")
     for (var i = 0; i < localName.length ; i++) {
        // console.log("i=  "+ i);
         
 
       if (i === line*22) {
-        console.log(i+ " "+ line*22+ " " + (i == line*22));
+      //  console.log(i+ " "+ line*22+ " " + (i == line*22));
         //  k=0;
-        line ++;console.log(i + " "+ localName.charAt(i)!= " ");
+       
+      //  console.log(i + " "+ localName.charAt(i)!= " ");
         if(localName.charAt(i)!= " "){
              
       
           debugger
-          let lastIndex = localName.lastIndexOf(" ");
+          let lastIndex = localName.substring(0, line*22).lastIndexOf(" ");
           
-          localName = localName.substring(0, lastIndex)+ "X" + localName.substring(lastIndex, localName.length);
-          
-     //   console.log("nameee " + localName);
+          finalText = finalText+  localName.substring(line-1, lastIndex)+ "X" + localName.substring(lastIndex, localName.length);
+           line ++;
+        console.log("nameee " + finalText);
         }//else{
          // this.name.concat("\n");
        // }
