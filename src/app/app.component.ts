63df0d8c641b42d60fccae8edb492b950b669dff
srkreddydendi <srkreddydendi@gmail.com>
2020-03-24 18:38:03 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,55 +7,39 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   name: string = "Angular 6";
-  
+
   ouput = "";
   colLenght = 22;
   //k:number = 0;
   print() {
-    let k=0;
-    let line =1;
-   let  finalText = "";
-    let localName = this.name;
-    localName = localName.replace(/\s/g, " ")
-    for (var i = 0; i < localName.length ; i++) {
-       // console.log("i=  "+ i);
-     
-      if (i === line*22) {
-      //  console.log(i+ " "+ line*22+ " " + (i == line*22));
-        //  k=0;
-       
-      //  console.log(i + " "+ localName.charAt(i)!= " ");
-        if(localName.charAt(i)!= " "){
-          let spaceIndex = 0
+    let k = 0;
+    let line = 1;
+    let finalText:string = "";
+    let localName:string = this.name;
+    localName = localName.replace(/\s/g, " ");
+    for (var i = 0; i < localName.length; i++) {
+      // console.log("i=  "+ i);
 
-            /* for(let j=0;j< i;j++){
-                if(localName.charAt[j] ==" "){
-                    spaceIndex = j;
-                }
-             }*/
-      
-        //  debugger
-          let lastIndex = localName.lastIndexOf(" ");
-          
-          
-         finalText =   finalText.substring(0, lastIndex)+ "X" + localName.substring(lastIndex, localName.length);
-           line ++;
-        console.log("nameee " + finalText);
-        }else{
-        finalText =  finalText + "\n";
+      if (i === line * 22) {
+        if (localName.charAt(i) != " ") {
+          debugger
+         let index =  localName.substring(i-22, i).lastIndexOf(" ")
+          let subStr = localName.substring(i-22, index).concat("X") + localName.substring(index, i);
+         // for(let s in subStr){
+           finalText.concat(subStr)
+          //}
+          //subStr[subStr.length-2].concat("X")
+          console.log();
+
+        } else {
+          finalText = finalText.concat(localName.substring(i-22, i).concat("X"));
         }
         //this.finalText = this.name;
-       // console.log(k);
-     //   console.log(name);
-      //  k = 0;
-      } else {
-       // k=i;
-       if(localName.charAt(line*22)!= " "){
-
-       }
-        finalText = finalText + localName.charAt(i);  
-
+        // console.log(k);
+        //   console.log(name);
+        //  k = 0;
       }
     }
+    console.log(finalText);
   }
 }
