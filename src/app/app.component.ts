cbdd0b6189767084575278f295c3f16e41cc9d48
srkreddydendi <srkreddydendi@gmail.com>
2020-03-24 19:29:02 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,17 +17,21 @@ export class AppComponent {
     let finalText:string = "";
     let localName:string = this.name;
     localName = localName.replace(/\s/g, " ");
+    let lastSpace = 0;
     for (var i = 0; i < localName.length; i++) {
       // console.log("i=  "+ i);
+      if(localName.charAt(i) == " "){
+        lastSpace = i;
+      }
 
       if (i === line * 22) {
         line++;
         if (localName.charAt(i) != " ") {
           debugger
          let str =  localName.substring(i-22, i);
-         var index = str.lastIndexOf(" ")
-         let a = localName.substring(i-22, index)
-         let b = localName.substring(index, i)
+        // var index = str.lastIndexOf(" ")
+         let a = localName.substring(i-22, lastSpace)
+         let b = localName.substring(lastSpace, i)
           let subStr = a+ "X" + b;
          // for(let s in subStr){
            finalText= finalText + subStr;
@@ -36,7 +40,7 @@ export class AppComponent {
         //  console.log(subStr);
 
         } else {
-        //  finalText = finalText+ (localName.substring(i-22, i).concat("X"));
+          finalText = finalText+ (localName.charAt(i));
         }
         //this.finalText = this.name;
         // console.log(k);
